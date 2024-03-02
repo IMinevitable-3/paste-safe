1d7b91899a17a718a41209ffb3ad613b4481dd7c
ks <komalsai130@gmail.com>
2024-03-02 21:23:50 +0530

    re render

--- paste_secure/build.sh
+++ paste_secure/build.sh
@@ -2,18 +2,11 @@
 # Exit on error
 set -o errexit
 
-# Specify Python version and use a virtual environment
-python -m venv venv
-source venv/bin/activate
-
-# Install dependencies using poetry
-poetry install
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
-
-# Deactivate the virtual environment
-deactivate
