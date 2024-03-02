2c1adc80047b85899767891839afa284a483fcf1
ks <komalsai130@gmail.com>
2024-03-02 20:52:37 +0530

    server config

--- paste_secure/environ.py
+++ paste_secure/environ.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 SECRET_KEY = os.environ.get("SECRET_KEY", "nothing")
 DEBUG = os.environ.get("DEBUG", True)
 PYTHON_VERSION = "3.11.4"
+POSTGRES_USER = os.environ.get("POSTGRES_USER", "user")
+POSTGRES_PASS = os.environ.get("POSTGRES_PASS", "password")
--- paste_secure/paste_secure/settings.py
+++ paste_secure/paste_secure/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -59,15 +62,28 @@ WSGI_APPLICATION = "paste_secure.wsgi.application"
 
 
 # Database
+from environ import POSTGRES_PASS, POSTGRES_USER
 
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         # Replace this value with your local database's connection string.
+#         default=f"postgres://{POSTGRES_USER}:{POSTGRES_PASS}@dpg-cnhdqn779t8c73eprh8g-a/paste_ezaf",
+#         conn_max_age=600,
+#     )
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default="postgresql://postgres:postgres@localhost:5432/paste_safe",
+        conn_max_age=600,
+    )
 }
-
 
 # Password validation
 
@@ -102,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
